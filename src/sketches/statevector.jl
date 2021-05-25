d58c428eaa376d62b2e151d1d9d6e16449ccf817
Miguel Alonso <miguel883@gmail.com>
2021-05-26 01:22:16 +0200

    More progress on staticvectors.jl

--- src/sketches/statevector.jl
+++ src/sketches/statevector.jl
@@ -3,63 +3,11 @@ module StateVector
 using BlockArrays, StaticArrays
 import BlockArrays: axes, viewblock, getblock
 
-#additions to export are not tracked by Revise. instead, until Julia is
-#restarted, we need to do Flight.LandingGear.ldg_demo
-export XLeaf, LBV, xleaf_demo, lbv_demo
+#additions to export are not tracked by Revise!
+export LBV, lbv_demo
 
 #setting default values directly:
 # https://mauro3.github.io/Parameters.jl/v0.9/manual.html
-
-struct XLeaf <: AbstractBlockVector{Float64}
-    data::BlockVector{Float64}
-    metadata::@NamedTuple{att::Int, vel::Int}
-    #supress all other constructors to ensure initial types and values are correct
-    function XLeaf()
-        data = mortar([rand(1), ones(2)])
-        #improvement: generate metadata automatically from the tuple of Symbols,
-        #simply adding their position automatically and putting everything in a
-        #Dict or NamedTuple?
-        metadata = (att = 1, vel = 2)
-        new(data, metadata)
-    end
-end
-
-#so, the first question is... what the fuck do i need to implement for a
-#concrete subtype of the AbstractBlockArray interface? by wading through the
-#source code in the BlockArrays repo it is very, very hard to tell. however, a
-#reasonable answer is: since BlockArray is a concrete subtype of
-#AbstractBlockArray, the methods required must have been implemented by
-#BlockArray. going to blockarray.jl, where BlockArray is defined, one finds
-#sections "AbstractBlockArray interface", "AbstractArray interface" and
-#"Indexing". if we reimplement this methods and simply forward the calls to the
-#BlockVector field, all should be fine
-#turns out the rest of methods are already implemented either by
-#AbstractBlockArray or AbstractArray and do what they need
-
-#these few could be forwarded automatically with a macro
-
-#AbstractBlockArray interface
-axes(x::XLeaf) = axes(x.data)
-viewblock(x::XLeaf, block) = viewblock(x.data, block)
-Base.getindex(x::XLeaf, blockindex::BlockIndex{1}) = getindex(x.data, blockindex) #not essential
-#AbstractArray interface
-Base.getindex(x::XLeaf, i::Integer) = getindex(x.data, i)
-Base.setindex!(x::XLeaf, v, i::Integer) = setindex!(x.data, v, i)
-
-
-function Base.getindex(x::XLeaf, s::Symbol)
-    return x.data[Block(x.metadata[s])]
-end
-
-function Base.setindex!(x::XLeaf, v, s::Symbol)
-    x.data[Block(x.metadata[s])] = v
-end
-
-#next, define XNode <: AbstractBlockVector{Float64} it must define the same
-#methods as XLeaf, but its constructor, instead of setting directly data and
-#metadata, it must receive an N tuple of AbstractBlockVector{Float64} and
-#another of Symbols/Strings. alternatively, we could receive a NamedTuple, see
-#what's better for type stability.
 
 #problem: since the keys of a named tuple are part of the type itself,
 #generating a NamedTuple for this, unless these keys can be inferred and
@@ -70,31 +18,6 @@ end
 #inferred directly from some type parameter:
 #https://docs.julialang.org/en/v1/manual/performance-tips/#man-performance-value-type
 
-# For instance, we get two instances of XLeaf() and two identifiers (:mlg, :nlg).
-#from the two XLeaf instances, we build another one using mortar and store it in
-#the data field. for the identifiers, we generate a metadata NamedTuple or Dict
-#mapping each now, the good news is that each block of this XNode is itself
-#either a XNode or a Leaf. and this means that it holds both its own sub-blocks
-#and their idents! so this solves the chained sub-block access by name!
-
-#now, thinking about the commonalities, the only difference between XNode and
-#XLeaf is where data and metadata come from. in XLeaf, it is hardcoded in the
-#constructor. in XNode they are given as arguments. other than that, the way
-#data and metadata are stored and accessed are the same. so it is not out of the
-#question to create a single BlockStateVector <: AbstractBlockVector. then,
-#there is no need for subclassing it any further. Each module that defines a
-#specific BlockStateVector Leaf simply defines a method that assembles it and
-#returns an instance of it! the only thing to care for is type stability: ensure
-#that the compiler has the information required. see above. either Dict or Val.
-#but probably a solution could be: leave the type of metadata open in the struct
-#declaration, then make sure to pass a hardcoded NamedTuple on each call to the
-#BlockStateVector constructor. if we use Symbols to generate the NT
-#automatically within the constructor, it will be type unstable, because the
-#constructor argument values (the Symbols) determine the NT type parameters. A
-#solution would be to use Val. to enforce having the same number of input blocks
-#and input labels, we could define them as NTuple{N, Symbol}, NTuple{N,
-#AbstractBlockVector} arguments
-
 #with all those pieces in place, it may be time to return to...
 # http://www.stochasticlifestyle.com/zero-cost-abstractions-in-julia-indexing-vectors-by-name-with-labelledarrays/
 #... adapting it for my  own case
@@ -104,6 +27,42 @@ end
 # more efficient, but it is more cumbersome and less flexible
 
 #DO NOT USE StaticArrays as blocks! their data cannot be referenced inside BlockVector
+
+#so, the first question is... what the fuck do i need to implement for a
+#concrete subtype of the AbstractBlockArray interface? by wading through the
+#source code in the BlockArrays repo it is very, very hard to tell. however, a
+#reasonable answer is: since BlockArray is a concrete subtype of
+#AbstractBlockArray, the methods required must have been implemented by
+#BlockArray. going to blockarray.jl, where BlockArray is defined, one finds
+#sections "AbstractBlockArray interface", "AbstractArray interface" and
+#"Indexing". if we reimplement this methods and simply forward the calls to the
+#BlockVector field, all should be fine turns out the rest of methods are already
+#implemented either by AbstractBlockArray or AbstractArray and do what they need
+
+
+
+# about integrating both Labels and BlockTypes in the type parameter itself...
+# it is not possible. Julia doc says:
+
+# Both abstract and concrete types can be parameterized by other types. They can
+# also be parameterized by symbols, by values of any type for which isbits
+# returns true (essentially, things like numbers and bools that are stored like
+# C types or structs with no pointers to other objects), and also by tuples
+# thereof. Type parameters may be omitted when they do not need to be referenced
+# or restricted.
+
+# so, i can use a tuple of symbols representing the labels for the different
+# blocks. but nothing more. so, maybe a cleaner solution would be to simply use
+# a Symbol identifier for the LBV that helps with dispatch. and then do what i
+# was doing: define a block descriptor for each LBV i want to define. then
+# dispatch to the correct getblocktype and getblocknumber using that Symbol
+# identifier
+# what i can do is:
+#for Aircraft:
+# getblockdescriptor(::LBV{:Aircraft}) = (:rbd = LBV{:Rbd}, :ldg = LBV{:Ldg})
+#and now, generic functions:
+# getblocknumber(T, s::Symbol) = findfirst(i->i==s, keys(getblockdescriptor(T)))
+# getblocktype(T, s::Symbol) = getblockdescriptor(T)[s]
 
 struct LBV{Labels} <: AbstractBlockVector{Float64}
     data::BlockVector{Float64}
@@ -116,9 +75,11 @@ struct LBV{Labels} <: AbstractBlockVector{Float64}
     end
 end
 
+#AbstractBlockArray interface
 axes(x::LBV) = axes(getfield(x,:data))
 viewblock(x::LBV, block) = viewblock(getfield(x, :data), block)
 
+#AbstractArray interface
 Base.getindex(x::LBV, i::Integer)::Float64 = getindex(getfield(x,:data), i)
 Base.getindex(x::LBV, i::Colon)::Vector{Float64} = getindex(getfield(x,:data),  i)
 Base.getindex(x::LBV, i::UnitRange{Int})::Vector{Float64} = getindex(getfield(x,:data),  i)
@@ -129,44 +90,17 @@ Base.setindex!(x::LBV, v, i::Colon) = setindex!(getfield(x,:data), v, i)
 Base.setindex!(x::LBV, v, i::UnitRange{Int}) = setindex!(getfield(x,:data), v, i)
 # Base.setindex!(x::LBV, v, i::Union{Integer, UnitRange{Int}, Colon}) = setindex!(getfield(x,:data), v, i)
 
-#one step further is to tell the compiler exactly WHICH type of LBV will that be
-#how? well, if i say that whenever i say x_aircraft[:rbd] the result will be of
-#the type LBV_Rbd = LBV{RbdLabels}, that's what i need
-
-
-const AircraftLabels = (:rbd, :ldg)
-const AircraftSV = LBV{AircraftLabels}
-
-const RbdLabels = (:att, :vel)
-const RbdSV = LBV{RbdLabels}
-
-const LdgLabels = (:nlg, :mlg)
-const LdgSV = LBV{LdgLabels}
-
-#problema: Si AttSV y VelSV no son sino alias para un Vector ordinario, no puede
-#haber confusion en los methods getindex y setindex con Val? NO! porque en
-#realidad nunca voy a llamar a esos methods si no es para un LBV! porque los
-#leafs no van a tener Labels. y por tanto no tiene sentido para ellos. asi que
-#basicamente no me molesto en definir AttSV y VelSV. todo lo que sea un leaf,
-#Vector{Float64} y sea acbo
-
-
 getblock(x::LBV, k::Integer) = getfield(x,:data)[Block(k)]
 
-Base.getindex(x::AircraftSV, ::Val{:rbd})::RbdSV = getblock(x, 1)
-Base.getindex(x::AircraftSV, ::Val{:ldg})::LdgSV = getblock(x, 2)
-Base.getproperty(x::AircraftSV, s::Symbol) = getindex(x, Val(s))
 
-Base.setindex!(x::AircraftSV, v, ::Val{:rbd}) = setindex!(getblock(x, 1), v, :)
+const RbdLabels = (:att, :vel, :pos)
+const LBVRbd = LBV{RbdLabels}
 
+const LdgLabels = (:nlg, :mlg)
+const LBVLdg = LBV{LdgLabels}
 
-Base.getindex(x::RbdSV, ::Val{:att})::Vector{Float64} = getblock(x, 1)
-Base.getindex(x::RbdSV, ::Val{:vel})::Vector{Float64} = getblock(x, 2)
-Base.getproperty(x::RbdSV, s::Symbol) = getindex(x, Val(s))
-
-Base.getindex(x::LdgSV, ::Val{:nlg})::Vector{Float64} = getblock(x, 1)
-Base.getindex(x::LdgSV, ::Val{:mlg})::Vector{Float64} = getblock(x, 2)
-Base.getproperty(x::LdgSV, s::Symbol) = getindex(x, Val(s))
+const AircraftLabels = (:rbd, :ldg)
+const LBVAircraft = LBV{AircraftLabels}
 
 # #all these need to be replaced by generated functions. from the value type S,
 # which is a #Symbol, it needs to find: which block number corresponds to it
@@ -174,42 +108,68 @@ Base.getproperty(x::LdgSV, s::Symbol) = getindex(x, Val(s))
 # LBV specific subtype corresponds to it (for example, to :rbd #corresponds
 # LBV_Rbd = LBV{RbdLabels}, to :ldg corresponds LBV_Ldg, etc
 
-# @generated function Base.getindex(x::LBV{CHANGETHISAircraftLabels}, ::Val{S}) where {S<:Symbol}
-#     blocknumber = getblocknumber(x, S) #this is a generated function so x is a type!
-#     blocktype = getblocktype(x, S)
-#     :(getblock($blocknumber)::$blocktype) #return it and enforce type
-# end
-
-# #must create functions getblocknumber & getblocktype that dispatch on the LBV
-# specific subtype as the first argument, and on the block label s on the
-# second. note that these functions must take a Type as a first argument, not a
-# value of a specified type. this is important, because within the generated
-# functions, variable names actually denote types. The blocktype can also be a
-# MVector, or whatever I want, as long as its constructor accepts the output of
-# getblock
-
 #the key to type stability is that these functions are called at compile time,
 # not at runtime, if they were called at runtime, since their output determines
 # the types of the extracted blocks... type instability!
 
 
-#THE NEXT STEP: PARAMETRIC TYPE!! Nope. YAGNI.
+# Base.getindex(x::LBVAircraft, ::Val{:rbd})::LBVRbd = getblock(x, 1)
+# Base.getindex(x::LBVAircraft, ::Val{:ldg})::LBVLdg = getblock(x, 2)
+# Base.getproperty(x::LBVAircraft, s::Symbol) = getindex(x, Val(s))
+
+# Base.getindex(x::LBVRbd, ::Val{:att})::Vector{Float64} = getblock(x, 1)
+# Base.getindex(x::LBVRbd, ::Val{:vel})::Vector{Float64} = getblock(x, 2)
+# Base.getproperty(x::LBVRbd, s::Symbol) = getindex(x, Val(s))
+
+# Base.getindex(x::LBVLdg, ::Val{:nlg})::Vector{Float64} = getblock(x, 1)
+# Base.getindex(x::LBVLdg, ::Val{:mlg})::Vector{Float64} = getblock(x, 2)
+# Base.getproperty(x::LBVLdg, s::Symbol) = getindex(x, Val(s))
+
+#these aren't necessary! once the slices or the complete block are retrieved,
+#the assignment changes them. but, since they are actually a reference to the
+#data held by the parent, this changes that as well.
+# Base.setindex!(x::LBVAircraft, v, ::Val{:rbd}) = setindex!(getblock(x, 1), v, :)
+# Base.setproperty!(x::LBVAircraft, v, s::Symbol) = setindex!(x, v, Val(s))
+
+Aircraft_block_descriptor = (rbd = LBVRbd, ldg = LBVLdg)
+getblocknumber(::Type{LBVAircraft}, s::Symbol) = findfirst(i->i==s, keys(Aircraft_block_descriptor))
+getblocktype(::Type{LBVAircraft}, s::Symbol) = Aircraft_block_descriptor[s]
+
+Rbd_block_descriptor = (att = MVector{4, Float64}, vel = MVector{3, Float64}, pos = MVector{3, Float64})
+getblocknumber(::Type{LBVRbd}, s::Symbol) = findfirst(i->i==s, keys(Rbd_block_descriptor))
+getblocktype(::Type{LBVRbd}, s::Symbol) = Rbd_block_descriptor[s]
+
+Ldg_block_descriptor = (nlg = Vector{Float64}, mlg = Vector{Float64})
+getblocknumber(::Type{LBVLdg}, s::Symbol) = findfirst(i->i==s, keys(Ldg_block_descriptor))
+getblocktype(::Type{LBVLdg}, s::Symbol) = Ldg_block_descriptor[s]
+
+@generated function Base.getindex(x::LBV, ::Val{s}) where {s}
+    #within the @generated function body, x is a type, and s is a Symbol (it is
+    #extracted from a type parameter). we could do LBV{T} where {T} and T would
+    #be also a Symbol type parameter, but we can dispatch directly on typeof(x)
+    Core.println("Generated function getindex parsed for type $x")
+    blocknumber = getblocknumber(x, s) #this is a generated function so x is a type!
+    blocktype = getblocktype(x, s)
+    :(getblock(getfield(x,:data), $blocknumber)::$blocktype) #return it and enforce type
+end
+Base.getproperty(x::LBV, s::Symbol) = getindex(x, Val(s))
 
 
 function lbv_demo()
-    att = Vector{Float64}(rand(4))
     att = MVector{4, Float64}(rand(4))
     vel = MVector{3, Float64}(2ones(3))
-    x_rbd = RbdSV((att, vel))
+    pos = MVector{3, Float64}(3ones(3))
+    x_rbd = LBVRbd((att, vel, pos))
 
     nlg = 5ones(3)
     mlg = 2ones(3)
-    x_ldg = LdgSV((nlg, mlg))
+    x_ldg = LBVLdg((nlg, mlg))
 
-    x_aircraft = AircraftSV((x_rbd, x_ldg))
+    x_aircraft = LBVAircraft((x_rbd, x_ldg))
     display(x_aircraft)
     x_rbd_retrieved = x_aircraft[Val(:rbd)]
-    x_rbd_retrieved[:] .= 0
+    x_rbd_retrieved[:] .= 111
+    x_aircraft.ldg .= 222
     display(x_aircraft)
 
     return (
@@ -218,7 +178,8 @@ function lbv_demo()
         x_aircraft[Val(:ldg)][:],
         x_aircraft[1:4],
         x_aircraft.rbd.att,
-        x_aircraft.ldg[:] .+ 2
+        x_aircraft.ldg[:],
+        # x_aircraft.ldg.nlg,
     )
 
 
@@ -269,18 +230,6 @@ end
 # realmente va a devolver un LBV, por lo que no sabe que la getindex a la que se
 # invoca despues corresponde realmente a LBV, y por tanto podria no ser esa a la
 # que le hemos restringido el return type
-
-
-
-function xleaf_demo()
-    # x_nlg = XLeaf()
-    # x_mlg = XLeaf()
-    # x = mortar([x_nlg, x_mlg])
-    # x[Block(2)][:att] = [π]
-    # x[Block(1)][:vel] = [11, 92.4]
-    # x1 = x[Block(1)][:att][1]
-    # display(x)
-end
 
 
 end #module
