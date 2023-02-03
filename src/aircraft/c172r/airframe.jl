a028ab269fa92cbd4d96e65281893d3184128417
Miguel Alonso <miguel883@gmail.com>
2023-02-03 11:47:58 +0100

    Airframe and avionics moved from c172r.jl to separate includes

---
+++ src/aircraft/c172r/airframe.jl
@@ -0,0 +1,450 @@
+using StaticArrays
+using ComponentArrays
+using Printf
+using CImGui, CImGui.CSyntax, CImGui.CSyntax.CStatic
+
+using Flight.Engine.Systems
+using Flight.Engine.GUI
+using Flight.Engine.Utils: Ranged, linear_scaling
+
+using Flight.Physics.Attitude
+using Flight.Physics.Kinematics
+using Flight.Physics.RigidBody
+
+using Flight.Components.Terrain
+using Flight.Components.Atmosphere
+using Flight.Components.LandingGear
+using Flight.Components.Propellers
+using Flight.Components.Piston
+using Flight.Components.Aircraft
+
+################################################################################
+############################ Airframe Subsystems ################################
+
+################################ Structure #####################################
+
+struct Structure <: Component end
+
+# This component represents the airframe structure, together with any components
+# rigidly attached to it, such as powerplant or landing gear, but not payload or
+# fuel contents. Its mass corresponds roughly to the aircraft's Standard Empty
+# Weight
+
+#Structure mass properties computed in the vehicle reference frame b
+const mp_Ob_str = let
+    #define the structure as a RigidBodyDistribution
+    str_G = RigidBodyDistribution(767.0, SA[820.0 0 0; 0 1164.0 0; 0 0 1702.0])
+    #define the transform from the origin of the vehicle reference frame (Ob)
+    #to the structure's center of mass (G)
+    t_Ob_G = FrameTransform(r = SVector{3}(0.056, 0, 0.582))
+    #compute the structure's mass properties at Ob
+    MassProperties(str_G, t_Ob_G)
+end
+
+RigidBody.MassTrait(::System{Structure}) = HasMass()
+
+#the structure itself receives no external actions. these are considered to act
+#upon the vehicle's aerodynamics, power plant and landing gear. the same goes
+#for rotational angular momentum.
+RigidBody.WrenchTrait(::System{Structure}) = GetsNoExternalWrench()
+RigidBody.AngMomTrait(::System{Structure}) = HasNoAngularMomentum()
+
+RigidBody.get_mp_Ob(::System{Structure}) = mp_Ob_str
+
+
+############################ Aerodynamics ######################################
+
+include("data/aero.jl")
+
+#the aircraft body reference frame fb is arbitrarily chosen to coincide with
+#the aerodynamics frame fa, so the frame transform is trivial
+const aero_lookup = generate_aero_lookup()
+const f_ba = FrameTransform()
+
+# if this weren't the case, and we cared not only about the rotation but also
+#about the velocity lever arm, here's the rigorous way of computing v_wOa_a:
+# v_wOb_b = v_eOb_b - v_ew_b
+# v_eOa_b = v_eOb_b + ω_eb_b × r_ObOa_b
+# v_wOa_b = v_eOa_b - v_ew_b = v_eOb_b + ω_eb_b × r_ObOa_b - v_ew_b
+# v_wOa_b = v_wOb_b + ω_eb_b × r_ObOa_b
+# v_wOa_a = q_ba'(v_wOa_b)
+
+Base.@kwdef struct AeroCoeffs
+    C_D::Float64 = 0.0
+    C_Y::Float64 = 0.0
+    C_L::Float64 = 0.0
+    C_l::Float64 = 0.0
+    C_m::Float64 = 0.0
+    C_n::Float64 = 0.0
+end
+
+function get_aero_coeffs(; α, β, p_nd, q_nd, r_nd, δa, δr, δe, δf, α_dot_nd, β_dot_nd, Δh_nd, stall)
+
+    #set sensible bounds
+    α = clamp(α, -0.1, 0.36) #0.36 is the highest value (post-stall) tabulated for C_L
+    β = clamp(β, -0.2, 0.2)
+    α_dot_nd = clamp(α_dot_nd, -0.04, 0.04)
+    β_dot_nd = clamp(β_dot_nd, -0.2, 0.2)
+
+    @unpack C_D, C_Y, C_L, C_l, C_m, C_n = aero_lookup
+
+    AeroCoeffs(
+        C_D = C_D.z + C_D.ge(Δh_nd) * (C_D.α_δf(α,δf) + C_D.δf(δf)) + C_D.δe(δe) + C_D.β(β),
+        C_Y = C_Y.δr * δr + C_Y.δa * δa + C_Y.β_δf(β,δf) + C_Y.p(α,δf) * p_nd + C_Y.r(α,δf) * r_nd,
+        C_L = C_L.ge(Δh_nd) * (C_L.α(α,stall) + C_L.δf(δf)) + C_L.δe * δe + C_L.q * q_nd + C_L.α_dot * α_dot_nd,
+        C_l = C_l.δa * δa + C_l.δr * δr + C_l.β * β + C_l.p * p_nd + C_l.r(α,δf) * r_nd,
+        C_m = C_m.z + C_m.δe * δe + C_m.δf(δf) + C_m.α * α + C_m.q * q_nd + C_m.α_dot * α_dot_nd,
+        C_n = C_n.δr * δr + C_n.δa * δa + C_n.β * β + C_n.p * p_nd + C_n.r * r_nd,
+    )
+
+end
+
+
+Base.@kwdef struct Aero <: AbstractAerodynamics
+    S::Float64 = 16.165 #wing area
+    b::Float64 = 10.912 #wingspan
+    c::Float64 = 1.494 #mean aerodynamic chord
+    δe_range::NTuple{2,Float64} = deg2rad.((-28, 23)) #elevator deflection range (rad)
+    δa_range::NTuple{2,Float64} = deg2rad.((-20, 20)) #aileron deflection range (rad)
+    δr_range::NTuple{2,Float64} = deg2rad.((-16, 16)) #rudder deflection range (rad)
+    δf_range::NTuple{2,Float64} = deg2rad.((0, 30)) #flap deflection range (rad)
+    α_stall::NTuple{2,Float64} = (0.09, 0.36) #α values for stall hysteresis switching
+    V_min::Float64 = 1.0 #lower airspeed threshold for non-dimensional angle rates
+    τ::Float64 = 0.05 #time constant for filtered airflow angle derivatives
+end
+
+#e↑ -> δe↑ -> trailing edge down -> Cm↓ -> pitch down
+#a↑ -> δa↑ -> left trailing edge down, right up -> Cl↑ -> roll right
+#r↑ -> δr↑ -> rudder trailing edge left -> Cn↓ -> yaw left
+#f↑ -> δf↑ -> flap trailing edge down -> CL↑
+
+Base.@kwdef mutable struct AeroU
+    e::Ranged{Float64, -1, 1} = 0.0
+    a::Ranged{Float64, -1, 1} = 0.0
+    r::Ranged{Float64, -1, 1} = 0.0
+    f::Ranged{Float64, 0, 1} = 0.0
+end
+
+Base.@kwdef mutable struct AeroS #discrete state
+    stall::Bool = false
+end
+
+Base.@kwdef struct AeroY
+    e::Float64 = 0.0 #normalized elevator control input
+    a::Float64 = 0.0 #normalized aileron control input
+    r::Float64 = 0.0 #normalized rudder control input
+    f::Float64 = 0.0 #normalized flap control input
+    α::Float64 = 0.0 #clamped AoA, aerodynamic axes
+    β::Float64 = 0.0 #clamped AoS, aerodynamic axes
+    α_filt::Float64 = 0.0 #filtered AoA
+    β_filt::Float64 = 0.0 #filtered AoS
+    α_filt_dot::Float64 = 0.0 #filtered AoA derivative
+    β_filt_dot::Float64 = 0.0 #filtered AoS derivative
+    stall::Bool = false #stall state
+    coeffs::AeroCoeffs = AeroCoeffs() #aerodynamic coefficients
+    wr_b::Wrench = Wrench() #aerodynamic Wrench, vehicle frame
+end
+
+Systems.init(::SystemX, ::Aero) = ComponentVector(α_filt = 0.0, β_filt = 0.0) #filtered airflow angles
+Systems.init(::SystemY, ::Aero) = AeroY()
+Systems.init(::SystemU, ::Aero) = AeroU()
+Systems.init(::SystemS, ::Aero) = AeroS()
+
+
+function Systems.f_ode!(sys::System{Aero}, ::System{<:Piston.Thruster},
+    air::AirData, kinematics::KinematicData, terrain::System{<:AbstractTerrain})
+
+    #for near-zero TAS, the airflow angles are likely to chatter between 0, -π
+    #and π. this can cause lots of noise in airflow angle derivatives and
+    #general unpleasantness. however, in this situation dynamic pressure will be
+    #close to zero, so forces and moments will vanish anyway.
+
+    @unpack ẋ, x, u, s, params = sys
+    @unpack α_filt, β_filt = x
+    @unpack e, a, r, f = u
+    @unpack S, b, c, δe_range, δa_range, δr_range, δf_range, α_stall, V_min, τ = params
+    @unpack TAS, q, v_wOb_b = air
+    @unpack ω_lb_b, n_e, h_o = kinematics
+    stall = s.stall
+
+    v_wOb_a = f_ba.q'(v_wOb_b)
+    α, β = get_airflow_angles(v_wOb_a)
+    V = max(TAS, V_min) #avoid division by zero
+
+    α_filt_dot = 1/τ * (α - α_filt)
+    β_filt_dot = 1/τ * (β - β_filt)
+
+    p_nd = ω_lb_b[1] * b / (2V) #non-dimensional roll rate
+    q_nd = ω_lb_b[2] * c / (2V) #non-dimensional pitch rate
+    r_nd = ω_lb_b[3] * b / (2V) #non-dimensional yaw rate
+
+    α_dot_nd = α_filt_dot * c / (2V)
+    β_dot_nd = β_filt_dot * b / (2V)
+
+    δe = linear_scaling(e, δe_range)
+    δa = linear_scaling(a, δa_range)
+    δr = linear_scaling(r, δr_range)
+    δf = linear_scaling(f, δf_range)
+
+    #non-dimensional height above ground
+    l2d_Oa = n_e #Oa = Ob
+    h_Oa = h_o #orthometric
+    h_trn_Oa = TerrainData(terrain, l2d_Oa).altitude #orthometric
+    Δh_nd = (h_Oa - h_trn_Oa) / b
+
+    # T = get_wr_b(pwp).F[1]
+    # C_T = T / (q * S) #thrust coefficient, not used here
+
+    coeffs = get_aero_coeffs(;
+        α, β, p_nd, q_nd, r_nd, δa, δr, δe, δf, α_dot_nd, β_dot_nd, Δh_nd, stall)
+
+    @unpack C_D, C_Y, C_L, C_l, C_m, C_n = coeffs
+
+    q_as = get_stability_axes(α)
+    F_aero_s = q * S * SVector{3,Float64}(-C_D, C_Y, -C_L)
+    F_aero_a = q_as(F_aero_s)
+    M_aero_a = q * S * SVector{3,Float64}(C_l * b, C_m * c, C_n * b)
+
+    # wr_b = wr_a = Wrench(F_aero_a, M_aero_a)
+    wr_b = Wrench(F_aero_a, M_aero_a)
+
+    ẋ.α_filt = α_filt_dot
+    ẋ.β_filt = β_filt_dot
+
+    sys.y = AeroY(; α, α_filt, α_filt_dot, β, β_filt, β_filt_dot,
+        e, a, r, f, stall, coeffs, wr_b)
+
+    return nothing
+
+end
+
+RigidBody.get_wr_b(sys::System{Aero}) = sys.y.wr_b
+
+function Systems.f_step!(sys::System{Aero})
+    #stall hysteresis
+    α = sys.y.α
+    α_stall = sys.params.α_stall
+    if α > α_stall[2]
+        sys.s.stall = true
+    elseif α < α_stall[1]
+        sys.s.stall = false
+    end
+    return false
+end
+
+# # splt_α = thplot(t, rad2deg.(α_b);
+# #     title = "Angle of Attack", ylabel = L"$\alpha \ (deg)$",
+# #     label = "", kwargs...)
+
+# # splt_β = thplot(t, rad2deg.(β_b);
+# #     title = "Angle of Sideslip", ylabel = L"$\beta \ (deg)$",
+# #     label = "", kwargs...)
+
+# # pd["05_α_β"] = plot(splt_α, splt_β;
+# #     plot_title = "Airflow Angles [Airframe]",
+# #     layout = (1,2),
+# #     kwargs..., plot_titlefontsize = 20) #override titlefontsize after kwargs
+
+
+
+############################# Landing Gear ####################################
+
+struct Ldg <: Component
+    left::LandingGearUnit{NoSteering, DirectBraking, Strut{SimpleDamper}}
+    right::LandingGearUnit{NoSteering, DirectBraking, Strut{SimpleDamper}}
+    nose::LandingGearUnit{DirectSteering, NoBraking, Strut{SimpleDamper}}
+end
+
+RigidBody.MassTrait(::System{Ldg}) = HasNoMass()
+RigidBody.WrenchTrait(::System{Ldg}) = GetsExternalWrench()
+RigidBody.AngMomTrait(::System{Ldg}) = HasNoAngularMomentum()
+
+function Ldg()
+
+    mlg_damper = SimpleDamper(k_s = 39404, #2700 lbf/ft
+                              k_d_ext = 9340, #640 lbf/(ft/s)
+                              k_d_cmp = 9340,
+                              F_max = 50000)
+    nlg_damper = SimpleDamper(k_s = 26269, #1800 lbf/ft
+                              k_d_ext = 3503, #240 lbf/(ft/s)
+                              k_d_cmp = 3503,
+                              F_max = 50000)
+
+    left = LandingGearUnit(
+        strut = Strut(
+            t_bs = FrameTransform(r = [-0.381, -1.092, 1.902], q = RQuat() ),
+            l_0 = 0.0,
+            damper = mlg_damper),
+        braking = DirectBraking())
+
+    right = LandingGearUnit(
+        strut = Strut(
+            t_bs = FrameTransform(r = [-0.381, 1.092, 1.902], q = RQuat() ),
+            l_0 = 0.0,
+            damper = mlg_damper),
+        braking = DirectBraking())
+
+    nose = LandingGearUnit(
+        strut = Strut(
+            t_bs = FrameTransform(r = [1.27, 0, 1.9] , q = RQuat()),
+            l_0 = 0.0,
+            damper = nlg_damper),
+        steering = DirectSteering())
+
+    Ldg(left, right, nose)
+
+end
+
+############################## Payload ######################################
+
+const pilot_slot = FrameTransform(r = SVector{3}(0.183, -0.356, 0.899))
+const copilot_slot = FrameTransform(r = SVector{3}(0.183, 0.356, 0.899))
+const psg_left_slot = FrameTransform(r = SVector{3}(-0.681, -0.356, 0.899))
+const psg_right_slot = FrameTransform(r = SVector{3}(-0.681, 0.356, 0.899))
+const baggage_slot = FrameTransform(r = SVector{3}(-1.316, 0, 0.899))
+
+struct Payload <: Component
+    pilot::MassProperties #mp_Ob
+    copilot::MassProperties #mp_Ob
+    psg_left::MassProperties #mp_Ob
+    psg_right::MassProperties #mp_Ob
+    baggage::MassProperties #mp_Ob
+
+    function Payload( ; pilot::PointDistribution = PointDistribution(75),
+                        copilot::PointDistribution = PointDistribution(75),
+                        psg_left::PointDistribution = PointDistribution(75),
+                        psg_right::PointDistribution = PointDistribution(75),
+                        baggage::PointDistribution = PointDistribution(50))
+
+        return new( MassProperties(pilot, pilot_slot),
+                    MassProperties(copilot, copilot_slot),
+                    MassProperties(psg_left, psg_left_slot),
+                    MassProperties(psg_right, psg_right_slot),
+                    MassProperties(baggage, baggage_slot)
+                    )
+
+    end
+end
+
+Base.@kwdef mutable struct PayloadU
+    pilot::Bool = true
+    copilot::Bool = true
+    psg_left::Bool = false
+    psg_right::Bool = false
+    baggage::Bool = true
+end
+
+Systems.init(::SystemU, ::Payload) = PayloadU()
+
+RigidBody.MassTrait(::System{Payload}) = HasMass()
+RigidBody.WrenchTrait(::System{Payload}) = GetsNoExternalWrench()
+RigidBody.AngMomTrait(::System{Payload}) = HasNoAngularMomentum()
+
+function RigidBody.get_mp_Ob(sys::System{Payload})
+    mp_Ob = MassProperties()
+    sys.u.pilot && (mp_Ob += sys.params.pilot)
+    sys.u.copilot && (mp_Ob += sys.params.copilot)
+    sys.u.psg_left && (mp_Ob += sys.params.psg_left)
+    sys.u.psg_right && (mp_Ob += sys.params.psg_right)
+    sys.u.baggage && (mp_Ob += sys.params.baggage)
+    return mp_Ob
+end
+
+
+############################## Fuel #########################################
+
+#assumes fuel is drawn equally from both tanks, no need to model them
+#individually for now
+Base.@kwdef struct Fuel <: Piston.AbstractFuelSupply
+    m_full::Float64 = 114.4 #maximum fuel mass (42 gal * 6 lb/gal * 0.454 kg/lb)
+    m_empty::Float64 = 0.0 #residual fuel mass
+end
+
+Base.@kwdef struct FuelY
+    m::Float64 = 0.0 #current fuel mass
+end
+
+#normalized fuel content (0: residual, 1: full)
+Systems.init(::SystemX, ::Fuel) = [0.5] #cannot be a scalar, need an AbstractVector{<:Real}
+Systems.init(::SystemY, ::Fuel) = FuelY()
+
+function Systems.f_ode!(sys::System{Fuel}, pwp::System{<:Piston.Thruster})
+
+    @unpack m_full, m_empty = sys.params #no need for subsystems
+    m = m_empty + sys.x[1] * (m_full - m_empty) #current mass
+    sys.ẋ .= -pwp.y.engine.ṁ / (m_full - m_empty)
+    sys.y = FuelY(m)
+
+end
+
+Piston.fuel_available(sys::System{<:Fuel}) = (sys.y.m > 0)
+
+function RigidBody.get_mp_Ob(fuel::System{Fuel})
+
+    #in case x accidentally becomes negative (fuel is consumed beyond x=0 before
+    #the engine dies)
+    m_fuel = max(0.0, fuel.y.m)
+
+    m_left = PointDistribution(0.5m_fuel)
+    m_right = PointDistribution(0.5m_fuel)
+
+    #fuel tanks reference frames
+    frame_left = FrameTransform(r = SVector{3}(0.325, -2.845, 0))
+    frame_right = FrameTransform(r = SVector{3}(0.325, 2.845, 0))
+
+    mp_Ob = MassProperties()
+    mp_Ob += MassProperties(m_left, frame_left)
+    mp_Ob += MassProperties(m_right, frame_right)
+
+    return mp_Ob
+end
+
+################################ Powerplant ####################################
+
+Pwp() = Piston.Thruster(propeller = Propeller(t_bp = FrameTransform(r = [2.055, 0, 0.833])))
+
+
+################################################################################
+################################ Airframe ######################################
+
+#P is introduced as a type parameter, because Piston.Thruster is itself a
+#parametric type, and therefore not concrete
+Base.@kwdef struct Airframe{P} <: AbstractAirframe
+    str::Structure = Structure()
+    aero::Aero = Aero()
+    ldg::Ldg = Ldg()
+    fuel::Fuel = Fuel()
+    pld::Payload = Payload()
+    pwp::P = Pwp()
+end
+
+
+################################################################################
+#################################### GUI #######################################
+
+
+function GUI.draw!(sys::System{<:Airframe}, gui_input::Bool = true)
+
+    @unpack pwp, ldg = sys
+
+    CImGui.Begin("Airframe (Cessna 172R)")
+
+    show_pwp = @cstatic check=false @c CImGui.Checkbox("Powerplant", &check)
+    if show_pwp
+        CImGui.Begin("Powerplant")
+            GUI.draw!(pwp, gui_input)
+        CImGui.End()
+    end
+
+
+    CImGui.Text("$(pwp.y.engine.state)")
+    CImGui.Text("$(pwp.y.engine.ω)")
+    # if CImGui.TreeNode("Powerplant")
+    #     GUI.draw!(pwp, gui_input)
+    #     CImGui.TreePop()
+    # end
+
+
+    CImGui.End()
+
+end
---
+++ src/aircraft/c172r/avionics.jl
@@ -0,0 +1,200 @@
+using Flight.Engine.Systems
+using Flight.Engine.IODevices
+using Flight.Engine.Joysticks
+using Flight.Engine.GUI
+using Flight.Engine.Utils: Ranged
+
+using Printf
+using CImGui, CImGui.CSyntax, CImGui.CSyntax.CStatic
+
+################################################################################
+########################### MechanicalControls #################################
+
+struct MechanicalControls <: AbstractAvionics end
+
+#elevator↑ (stick forward) -> e↑ -> δe↑ -> trailing edge down -> Cm↓ -> pitch down
+#aileron↑ (stick right) -> a↑ -> δa↑ -> left trailing edge down, right up -> Cl↓ -> roll right
+#rudder↑ (right pedal forward) -> r↓ -> δr↓ -> rudder trailing edge right -> Cn↑ -> yaw right
+#rudder↑ (right pedal forward) -> nose wheel steering right -> yaw right
+#flaps↑ -> δf↑ -> flap trailing edge down -> CL↑
+Base.@kwdef mutable struct MechanicalControlsU
+    throttle::Ranged{Float64, 0, 1} = 0.0
+    aileron_trim::Ranged{Float64, -1, 1} = 0.0
+    aileron_offset::Ranged{Float64, -1, 1} = 0.0 #incremental command, for input devices
+    elevator_trim::Ranged{Float64, -1, 1} = 0.0
+    elevator_offset::Ranged{Float64, -1, 1} = 0.0 #incremental command, for input devices
+    rudder_trim::Ranged{Float64, -1, 1} = 0.0
+    rudder_offset::Ranged{Float64, -1, 1} = 0.0 #incremental command, for input devices
+    brake_left::Ranged{Float64, 0, 1} = 0.0
+    brake_right::Ranged{Float64, 0, 1} = 0.0
+    flaps::Ranged{Float64, 0, 1} = 0.0
+    mixture::Ranged{Float64, 0, 1} = 0.5
+    eng_start::Bool = false
+    eng_stop::Bool = false
+end
+
+Base.@kwdef struct MechanicalControlsY
+    throttle::Float64 = 0.0
+    aileron_trim::Float64 = 0.0
+    aileron_offset::Float64 = 0.0
+    elevator_trim::Float64 = 0.0
+    elevator_offset::Float64 = 0.0
+    rudder_trim::Float64 = 0.0
+    rudder_offset::Float64 = 0.0
+    brake_left::Float64 = 0.0
+    brake_right::Float64 = 0.0
+    flaps::Float64 = 0.0
+    mixture::Float64 = 0.5
+    eng_start::Bool = false
+    eng_stop::Bool = false
+end
+
+Systems.init(::SystemU, ::MechanicalControls) = MechanicalControlsU()
+Systems.init(::SystemY, ::MechanicalControls) = MechanicalControlsY()
+
+
+########################### Update Methods #####################################
+
+function Systems.f_ode!(avionics::System{MechanicalControls}, ::System{<:Airframe},
+                ::KinematicData, ::AirData, ::System{<:AbstractTerrain})
+
+    #MechanicalControls has no internal dynamics, just input-output feedthrough
+    @unpack throttle, aileron_trim, aileron_offset, elevator_trim, elevator_offset,
+            rudder_trim, rudder_offset, brake_left, brake_right, flaps, mixture,
+            eng_start, eng_stop = avionics.u
+
+    avionics.y = MechanicalControlsY(;
+            throttle, aileron_trim, aileron_offset, elevator_trim, elevator_offset,
+            rudder_trim, rudder_offset, brake_left, brake_right, flaps, mixture,
+            eng_start, eng_stop)
+
+end
+
+#no digital components or state machines in MechanicalControls
+@inline Systems.f_step!(::System{MechanicalControls}, ::System{<:Airframe}, ::KinematicSystem) = false
+@inline Systems.f_disc!(::System{MechanicalControls}, ::System{<:Airframe}, ::KinematicSystem, Δt) = false
+
+
+function map_controls!(airframe::System{<:Airframe}, avionics::System{MechanicalControls})
+
+    @unpack throttle, aileron_trim, aileron_offset, elevator_trim, elevator_offset,
+            rudder_trim, rudder_offset, brake_left, brake_right, flaps, mixture,
+            eng_start, eng_stop = avionics.u
+
+    @unpack aero, pwp, ldg = airframe
+
+    pwp.u.engine.start = eng_start
+    pwp.u.engine.stop = eng_stop
+    pwp.u.engine.thr = throttle
+    pwp.u.engine.mix = mixture
+    ldg.u.nose.steering[] = (rudder_trim + rudder_offset) #rudder↑ (right pedal forward) -> nose wheel steering right
+    ldg.u.left.braking[] = brake_left
+    ldg.u.right.braking[] = brake_right
+    aero.u.e = (elevator_trim + elevator_offset) #elevator↑ (stick forward) -> e↑ -> pitch down
+    aero.u.a = (aileron_trim + aileron_offset) #aileron↑ (stick right) -> a↑ -> roll right
+    aero.u.r = -(rudder_trim + rudder_offset) #rudder↑ (right pedal forward) -> r↓ -> yaw right
+    aero.u.f = flaps #flaps↑ -> δf↑
+
+    return nothing
+end
+
+
+############################ Joystick Mappings #################################
+
+elevator_curve(x) = exp_axis_curve(x, strength = 1, deadzone = 0.05)
+aileron_curve(x) = exp_axis_curve(x, strength = 1, deadzone = 0.05)
+rudder_curve(x) = exp_axis_curve(x, strength = 1.5, deadzone = 0.05)
+brake_curve(x) = exp_axis_curve(x, strength = 1, deadzone = 0.05)
+
+function IODevices.assign!(u::MechanicalControlsU,
+            joystick::Joystick{XBoxControllerID}, ::DefaultMapping)
+
+    u.aileron_offset = get_axis_value(joystick, :right_analog_x) |> aileron_curve
+    u.elevator_offset = -get_axis_value(joystick, :right_analog_y) |> elevator_curve
+    u.rudder_offset = get_axis_value(joystick, :left_analog_x) |> rudder_curve
+    u.brake_left = get_axis_value(joystick, :left_trigger) |> brake_curve
+    u.brake_right = get_axis_value(joystick, :right_trigger) |> brake_curve
+
+    u.aileron_trim -= 0.01 * was_released(joystick, :dpad_left)
+    u.aileron_trim += 0.01 * was_released(joystick, :dpad_right)
+    u.elevator_trim -= 0.01 * was_released(joystick, :dpad_down)
+    u.elevator_trim += 0.01 * was_released(joystick, :dpad_up)
+
+    u.throttle += 0.1 * was_released(joystick, :button_Y)
+    u.throttle -= 0.1 * was_released(joystick, :button_A)
+
+    u.flaps += 0.3333 * was_released(joystick, :right_bumper)
+    u.flaps -= 0.3333 * was_released(joystick, :left_bumper)
+
+end
+
+
+################################## GUI #########################################
+
+macro input_slider(target, label, lower_bound, upper_bound, default)
+    return esc(quote
+        #generate a running plot
+        let
+            @cstatic values=fill(Cfloat(0),90) values_offset=Cint(0) begin
+                values[values_offset+1] = $target
+                values_offset = (values_offset+1) % length(values)
+                CImGui.PlotLines(string($target |> Float32), values, length(values), values_offset, $label, 0.0, 1.0, (0,50))
+            end
+            #generate a checkbox-enabled slider to control the target's value
+            if gui_input
+                enable_flag = @cstatic check=false @c CImGui.Checkbox("Enable##"*($label), &check)
+                CImGui.SameLine()
+                slider_value = @cstatic f=Cfloat($default) @c CImGui.SliderFloat("##"*($label), &f, $lower_bound, $upper_bound)
+                enable_flag && ($target = slider_value)
+            end
+        end
+    end)
+end
+
+macro input_button(target, label, hue)
+    return esc(quote
+        let
+            if gui_input
+                enable_flag = @cstatic check=false @c CImGui.Checkbox("Enable##"*($label), &check)
+                CImGui.SameLine()
+                CImGui.PushStyleColor(CImGui.ImGuiCol_Button, CImGui.HSV($hue, 0.6, 0.6))
+                CImGui.PushStyleColor(CImGui.ImGuiCol_ButtonHovered, CImGui.HSV($hue, 0.7, 0.7))
+                CImGui.PushStyleColor(CImGui.ImGuiCol_ButtonActive, CImGui.HSV($hue, 0.8, 0.8))
+                CImGui.Button(($label))
+                CImGui.PopStyleColor(3)
+                enable_flag && ($target = CImGui.IsItemActive())
+            else
+                CImGui.Text(($label) * ": " *string($target))
+            end
+        end
+    end)
+end
+
+
+function GUI.draw!(sys::System{<:MechanicalControls}, gui_input::Bool = true)
+
+    u = sys.u
+
+    CImGui.Begin("Avionics (Cessna 172R / Mechanical Controls)")
+
+    CImGui.PushItemWidth(-60)
+
+    @input_button(u.eng_start, "Engine Start", 0.4)
+    @input_button(u.eng_stop, "Engine Stop", 0.0)
+    @input_slider(u.throttle, "Throttle", 0, 1, 1)
+    @input_slider(u.mixture, "Mixture", 0, 1, 0.5)
+    @input_slider(u.brake_left, "Left Brake", 0, 1, 0.0)
+    @input_slider(u.brake_right, "Right Brake", 0, 1, 0.0)
+    @input_slider(u.aileron_offset, "Aileron Offset", -1, 1, 0.0)
+    @input_slider(u.elevator_offset, "Elevator Offset", -1, 1, 0.0)
+    @input_slider(u.rudder_offset, "Rudder Offset", -1, 1, 0.0)
+    @input_slider(u.flaps, "Flaps", 0, 1, 0.0)
+    @input_slider(u.aileron_trim, "Aileron Trim", -1, 1, 0.0)
+    @input_slider(u.elevator_trim, "Elevator Trim", -1, 1, 0.0)
+    @input_slider(u.rudder_trim, "Rudder Trim", -1, 1, 0.0)
+
+    CImGui.PopItemWidth()
+
+    CImGui.End()
+
+end
