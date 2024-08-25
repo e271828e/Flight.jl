7e31387b77f5c58a5503060f58da4b665af8bd31
Miguel Alonso <miguel883@gmail.com>
2024-08-25 21:20:44 +0200

    Updating Dynamics

--- src/physics/FlightPhysics.jl
+++ src/physics/FlightPhysics.jl
@@ -6,8 +6,8 @@ include("quaternions.jl"); @reexport using .Quaternions
 include("attitude.jl"); @reexport using .Attitude
 include("geodesy.jl"); @reexport using .Geodesy
 include("kinematics.jl"); @reexport using .Kinematics
-# include("dynamics.jl"); @reexport using .Dynamics
-# include("atmosphere.jl"); @reexport using .Atmosphere
-# include("terrain.jl"); @reexport using .Terrain
+include("dynamics.jl"); @reexport using .Dynamics
+include("atmosphere.jl"); @reexport using .Atmosphere
+include("terrain.jl"); @reexport using .Terrain
 
 end
--- src/physics/dynamics.jl
+++ src/physics/dynamics.jl
@@ -19,7 +19,9 @@ export DynamicsData
 #standard gravity for specific force normalization
 const g₀ = 9.80665
 
-############################# FrameTransform ###############################
+
+################################################################################
+############################# FrameTransform ###################################
 
 """
 Specifies a reference frame `fc(Oc, Ɛc)` relative to another `fb(Ob, Ɛb)`
@@ -33,7 +35,6 @@ Frame `fc(Oc, Ɛc)` is defined by:
     r::SVector{3,Float64} = zeros(SVector{3})
     q::RQuat = RQuat()
 end
-
 
 """
     Base.:∘(t_bc::FrameTransform, t_cd::FrameTransform)
@@ -53,7 +54,6 @@ function Base.:∘(t_bc::FrameTransform, t_cd::FrameTransform)
     return t_bd
 
 end
-
 
 """
     Base.:adjoint(t_bc::FrameTransform)
@@ -93,7 +93,8 @@ end
 
 
 
-############################## Wrench #################################
+################################################################################
+################################## Wrench ######################################
 
 """
 Force and torque combination defined on a concrete reference frame
@@ -146,7 +147,8 @@ function transform(t_bc::FrameTransform, wr_c::Wrench)
 end
 
 
-############################## MassDistributions #################################
+################################################################################
+############################## MassDistributions ###############################
 
 abstract type AbstractMassDistribution end
 
@@ -163,10 +165,14 @@ principal axes of inertia)
 - m: Total mass
 - J: Inertia tensor computed with respect to Oc and projected in εc
 """
-struct RigidBodyDistribution <: AbstractMassDistribution
-    m::Float64
-    J::SMatrix{3,3,Float64,9}
+@kwdef struct RigidBodyDistribution <: AbstractMassDistribution
+    m::Float64 = 1.0
+    J::SMatrix{3,3,Float64,9} = diagm(ones(SVector{3}))
 end
+
+
+################################################################################
+################################ MassProperties ################################
 
 
 """
@@ -297,13 +303,12 @@ end
 
 ############################ MassTrait #################################
 
-abstract type MassTrait end
-struct HasMass <: MassTrait end
-struct HasNoMass <: MassTrait end
+################################################################################
+############################### RigidBodyData ##################################
 
 """
 Notes:
-- When get_mp_Ob is called on a System, the returned MassProperties instance must
+- When get_mp_b is called on a System, the returned MassProperties instance must
   be expressed in the System's parent reference frame.
 - At the root of the component hierarchy we have the vehicle. The vehicle is
   its own parent, so the MassProperties it returns must be expressed in its own
@@ -318,12 +323,32 @@ Notes:
   worry about discontinuities in the kinematic state vector.
 """
 
-MassTrait(::S) where {S<:System} = error(
-    "Please extend Dynamics.MassTrait for $S")
+#mp_Ob: Mass properties of the System, translated to the vehicle frame
 
-get_mp_Ob(sys::System) = get_mp_Ob(MassTrait(sys), sys)
+#wr_ext_Ob: Total external wrench contributed to the System, resolved in the
+#vehicle frame
 
-get_mp_Ob(::HasNoMass, sys::System) = MassProperties()
+#hr_b: Internal angular momentum due to the angular velocity of the rotating
+#System's components with respect to the vehicle. Computed individually by
+#each component relative to its center of mass and then summed
+
+#note: for a given J_Ob_b, r_ObG_b cannot be arbitrarily large, because
+#moments of inertia are minimum at G. therefore, at some point J_G_b would
+#become non-positive definite
+
+@kwdef struct RigidBodyData
+    mp_Ob::MassProperties = MassProperties(RigidBodyDistribution())
+    wr_ext_Ob::Wrench = Wrench()
+    hr_b::SVector{3,Float64} = zeros(SVector{3})
+end
+
+#default implementation
+function RigidBodyData(sys::System)
+    mp_Ob = get_mp_b(sys)
+    wr_ext_Ob = get_wr_b(sys)
+    hr_b = get_hr_b(sys)
+    RigidBodyData(mp_Ob, wr_ext_Ob, hr_b)
+end
 
 #default implementation for a System with the HasMass trait tries to compute
 #the aggregate mass properties for all its the subsystems
@@ -457,7 +482,7 @@ function inertia_wrench(mp_Ob::MassProperties, kin::KinData, hr_b::AbstractVecto
     #total angular momentum
     h_all_b = h_rbd_b + SVector{3,Float64}(hr_b)
 
-    #inertia terms (exact... overkill, but very cheap anyway)
+    #inertia terms (exact version... overkill, but very cheap anyway)
     a_1_b = (ω_eb_b + 2 * ω_ie_b) × v_eOb_b
     F_in_Ob_b = -m * (a_1_b + ω_ib_b × (ω_ib_b × r_ObG_b) + r_ObG_b × (ω_eb_b × ω_ie_b ))
     M_in_Ob_b = - ( J_Ob_b * (ω_ie_b × ω_eb_b) + ω_ib_b × h_all_b + m * r_ObG_b × a_1_b)
@@ -497,15 +522,20 @@ function gravity_wrench(mp_Ob::MassProperties, kin::KinData)
 end
 
 
-###################### Acceleration Outputs #####################
+################################################################################
+############################### Dynamics #######################################
+
+struct RigidBodyDynamics <: SystemDefinition end
 
 #all magnitudes resolved in body axes unless otherwise noted
-@kwdef struct DynamicsData
+@kwdef struct RigidBodyDynamicsY
     mp_Ob::MassProperties = MassProperties() #aircraft mass properties at Ob
+    mp_Gb::MassProperties = MassProperties() #aircraft mass properties at G
     wr_g_Ob::Wrench = Wrench() #gravity wrench at Ob
     wr_in_Ob::Wrench = Wrench() #inertia wrench at Ob
     wr_ext_Ob::Wrench = Wrench() #externally applied wrench at Ob
     wr_net_Ob::Wrench = Wrench() #net wrench at Ob
+    wr_net_Gb::Wrench = Wrench() #net wrench at G
     hr_b::SVector{3,Float64} = zeros(SVector{3}) #angular momentum from rotating components
     α_eb_b::SVector{3,Float64} = zeros(SVector{3}) #ECEF-to-body angular acceleration
     α_ib_b::SVector{3,Float64} = zeros(SVector{3}) #ECI-to-body angular acceleration
@@ -513,32 +543,18 @@ end
     a_eOb_b::SVector{3,Float64} = zeros(SVector{3}) #ECEF-relative acceleration of Ob
     a_eOb_n::SVector{3,Float64} = zeros(SVector{3}) #ECEF-relative acceleration of Ob, NED axes
     a_iOb_b::SVector{3,Float64} = zeros(SVector{3}) #ECI-relative acceleration of Ob
-    f_Ob_b::SVector{3,Float64} = zeros(SVector{3}) #specific force at Ob
     a_iG_b::SVector{3,Float64} = zeros(SVector{3}) #ECI-relative acceleration of G
-    f_G_b::SVector{3,Float64} = zeros(SVector{3}) #specific force at G
+    f_Ob_b::SVector{3,Float64} = zeros(SVector{3}) #specific force at Ob
+    f_Gb_b::SVector{3,Float64} = zeros(SVector{3}) #specific force at G
 end
 
-function update!(ẋ_vel::Kinematics.XVel, kin::KinData, mp_Ob::MassProperties,
-    wr_ext_Ob::Wrench, hr_b::AbstractVector{<:Real})
+Systems.X(::RigidBodyDynamics) = zero(Kinematics.XVelTemplate)
+Systems.Y(::RigidBodyDynamics) = RigidBodyDynamicsY()
 
-    #wr_ext_Ob: Total external wrench on the vehicle
+function Systems.f_ode!(sys::System{RigidBodyDynamics}, kin_data::KinData, rb_data::RigidBodyData)
 
-    #hr_b: Additional angular momentum due to the angular velocity of the
-    #rotating aircraft components with respect to the vehicle. these are
-    #computed individually by each component relative to its center of mass and
-    #then summed
-
-    #wr_ext_Ob and hr_b, as well as mass data, are produced by aircraft
-    #components, so they must be computed by the aircraft's x_dot method. and,
-    #since kin is needed by those components, it must be called from the
-    #aircraft's kinematic state vector
-
-    #note: for a given J_Ob_b, r_ObG_b cannot be arbitrarily large, because
-    #moments of inertia are minimum at G. therefore, at some point J_G_b would
-    #become non-positive definite
-
-    @unpack q_eb, q_nb, n_e, h_e, r_eOb_e, ω_eb_b, ω_ie_b, ω_ib_b, v_eOb_b = kin
-
+    @unpack q_eb, q_nb, n_e, h_e, r_eOb_e, ω_eb_b, ω_ie_b, ω_ib_b, v_eOb_b = kin_data
+    @unpack mp_Ob, wr_ext_Ob, hr_b = rb_data
     m = mp_Ob.m; J_Ob_b = mp_Ob.J_O; r_ObG_b = mp_Ob.r_OG
 
     r_ObG_b_sk = Attitude.v2skew(r_ObG_b)
@@ -549,39 +565,47 @@ function update!(ẋ_vel::Kinematics.XVel, kin::KinData, mp_Ob::MassProperties,
 
     A = vcat(hcat(A11, A12), hcat(A21, A22))
 
-    wr_g_Ob = gravity_wrench(mp_Ob, kin)
-    wr_in_Ob = inertia_wrench(mp_Ob, kin, hr_b)
+    wr_g_Ob = gravity_wrench(mp_Ob, kin_data)
+    wr_in_Ob = inertia_wrench(mp_Ob, kin_data, hr_b)
     wr_net_Ob = wr_ext_Ob + wr_g_Ob + wr_in_Ob
     b = SVector{6}(vcat(wr_net_Ob.M, wr_net_Ob.F))
 
-    # update ẋ_vel
-    ẋ_vel .= A\b
+    sys.ẋ = A\b
 
-    #compute outputs
+    wr_g_Ob = gravity_wrench(mp_Ob, kin_data)
+    wr_in_Ob = inertia_wrench(mp_Ob, kin_data, hr_b)
+    wr_net_Ob = wr_ext_Ob + wr_g_Ob + wr_in_Ob
+
     Ob = Geographic(n_e, h_e)
     r_eOb_b = q_eb'(r_eOb_e)
-    v̇_eOb_b = SVector{3}(ẋ_vel.v_eOb_b)
+    v̇_eOb_b = SVector{3}(ẋ.v_eOb_b)
 
     #angular accelerations
-    α_eb_b = SVector{3}(ẋ_vel.ω_eb_b) #α_eb_b == ω_eb_b_dot
+    α_eb_b = SVector{3}(ẋ.ω_eb_b) #α_eb_b == ω_eb_b_dot
     α_ib_b = α_eb_b - ω_eb_b × ω_ie_b
 
     #linear accelerations and specific force
     a_eOb_b = v̇_eOb_b + ω_eb_b × v_eOb_b
     a_eOb_n = q_nb(a_eOb_b)
     a_iOb_b = v̇_eOb_b + (ω_eb_b + 2ω_ie_b) × v_eOb_b + ω_ie_b × (ω_ie_b × r_eOb_b)
-    a_iG_b = a_iOb_b + ω_ib_b × (ω_ib_b × r_ObG_b) + α_ib_b × r_ObG_b
-
     g_Ob_b = q_nb'(g_n(Ob))
     G_Ob_b = g_Ob_b + ω_ie_b × (ω_ie_b × r_eOb_b)
     f_Ob_b = a_iOb_b - G_Ob_b
-    f_G_b = a_iG_b - G_Ob_b #G ≈ Ob
 
-    return DynamicsData(; mp_Ob, wr_g_Ob, wr_in_Ob, wr_ext_Ob, wr_net_Ob, hr_b,
-                           α_eb_b, α_ib_b, v̇_eOb_b, a_eOb_b, a_eOb_n,
-                           a_iOb_b, f_Ob_b, a_iG_b, f_G_b)
+    #translated quantities
+    r_ObG_b = mp_Ob.r_OG
+    t_GbOb = FrameTransform(r = -r_ObG_b) #Gb to Ob
+    mp_Gb = t_GbOb(mp_Ob)
+    wr_net_Gb = t_GbOb(wr_net_Ob)
+    a_iG_b = a_iOb_b + ω_ib_b × (ω_ib_b × r_ObG_b) + α_ib_b × r_ObG_b
+    f_Gb_b = a_iG_b - G_Ob_b #G ≈ Ob
+
+    sys.y = RigidBodyDynamicsY(; mp_Ob, mp_Gb, wr_g_Ob, wr_in_Ob, wr_ext_Ob,
+        wr_net_Ob, wr_net_Gb, hr_b, α_eb_b, α_ib_b, v̇_eOb_b, a_eOb_b, a_eOb_n,
+        a_iOb_b, a_iG_b, f_Ob_b, f_Gb_b)
 
 end
+
 
 ################################# Dynamics #####################################
 
@@ -608,7 +632,7 @@ end
 
 end
 
-function Plotting.make_plots(ts::TimeSeries{<:DynamicsData}; kwargs...)
+function Plotting.make_plots(ts::TimeSeries{<:RigidBodyDynamicsY}; kwargs...)
 
     pd = OrderedDict{Symbol, Plots.Plot}()
 
@@ -677,7 +701,7 @@ function Plotting.make_plots(ts::TimeSeries{<:DynamicsData}; kwargs...)
         ts_split = :h, link = :none,
         kwargs...)
 
-    pd[:f_G_b] = plot(TimeSeries(ts._t, ts.f_G_b._data / g₀);
+    pd[:f_Gb_b] = plot(TimeSeries(ts._t, ts.f_Gb_b._data / g₀);
         plot_title = "Specific Force (Center of Mass) [Vehicle Axes]",
         ylabel = hcat(
             L"$f_{G}^{x_b} \ (g)$",
@@ -693,14 +717,10 @@ end
 ################################################################################
 ################################# GUI ##########################################
 
-function GUI.draw(rb::DynamicsData, p_open::Ref{Bool} = Ref(true),
+function GUI.draw(sys::System{RigidBodyDynamics}, p_open::Ref{Bool} = Ref(true),
                     label::String = "Rigid Body")
 
-    @unpack mp_Ob, wr_net_Ob, hr_b, α_eb_b, a_eOb_b, f_Ob_b, f_G_b = rb
-
-    t_GbOb = FrameTransform(r = -mp_Ob.r_OG) #Gb to Ob
-    mp_Gb = t_GbOb(mp_Ob)
-    wr_net_Gb = t_GbOb(wr_net_Ob)
+    @unpack mp_Ob, mp_Gb, wr_net_Ob, hr_b, α_eb_b, a_eOb_b, f_Ob_b, f_Gb_b = sys.y
 
     CImGui.Begin(label, p_open)
 
@@ -724,7 +744,7 @@ function GUI.draw(rb::DynamicsData, p_open::Ref{Bool} = Ref(true),
     GUI.draw(hr_b, "Angular Momentum From Rotating Components [Body]", "kg*(m^2)/s")
     GUI.draw(α_eb_b, "Angular Acceleration (Body / ECEF) [Body]", "rad/(s^2)")
     GUI.draw(a_eOb_b, "Linear Acceleration (O / ECEF) [Body]", "m/(s^2)")
-    GUI.draw(f_G_b/g₀, "Specific Force (G) [Body]", "g")
+    GUI.draw(f_Gb_b/g₀, "Specific Force (G) [Body]", "g")
 
     CImGui.End()
 
