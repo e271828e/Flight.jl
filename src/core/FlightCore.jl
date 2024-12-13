8c2a7bbe389831e72a5324a6252249e8e2aa3dd4
Miguel Alonso <miguel883@gmail.com>
2024-12-13 12:31:48 +0100

    Updated Project.toml and .gitignore

--- src/core/FlightCore.jl
+++ src/core/FlightCore.jl
@@ -4,11 +4,11 @@ using Reexport
 
 include("iodevices.jl"); @reexport using .IODevices
 include("gui.jl"); @reexport using .GUI
-include("utils.jl"); @reexport using .Utils
-include("systems.jl"); @reexport using .Systems
-include("sim.jl"); @reexport using .Sim
-include("plotting.jl"); @reexport using .Plotting
-include("network.jl"); @reexport using .Network
-include("joysticks.jl"); @reexport using .Joysticks
+# include("utils.jl"); @reexport using .Utils
+# include("systems.jl"); @reexport using .Systems
+# include("sim.jl"); @reexport using .Sim
+# include("plotting.jl"); @reexport using .Plotting
+# include("network.jl"); @reexport using .Network
+# include("joysticks.jl"); @reexport using .Joysticks
 
 end
