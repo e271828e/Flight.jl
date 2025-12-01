8681af3d227f352b9519757ef466b8f9d297b832
Miguel Alonso <miguel883@gmail.com>
2025-12-01 21:03:09 +0100

    Revised implementation of SBR Vehicle

--- src/examples/sbr/sbr.jl
+++ src/examples/sbr/sbr.jl
@@ -9,7 +9,7 @@ using Flight.FlightLib
 const g = 9.80665 #m/s^2, standard gravity
 
 ################################################################################
-################################## Vehicle #####################################
+################################## Vehicle2 #####################################
 
 #body 1: main body, comprising the vehicle chassis and the DC motor's case and stator
 #body 2: rolling body, comprising the wheel, axle, and the DC motor's rotor.
@@ -17,29 +17,27 @@ const g = 9.80665 #m/s^2, standard gravity
 @kwdef struct Vehicle <: ModelDefinition
     L::Float64 = 0.15 #distance from main body's origin to its CoM (m)
     R::Float64 = 0.05 #wheel radius (m)
-    m1::Float64 = 0.5 #mass of main body (kg)
-    m2::Float64 = 0.1 #mass of rolling body (kg)
+    m_1::Float64 = 0.5 #mass of main body (kg)
+    m_2::Float64 = 0.1 #mass of rolling body (kg)
+    J_1::Float64 = 1/12 * m_1 * (2L)^2 #main body's moment of inertia with respect to its CoM (kg*m^2)
+    J_2::Float64 = 1/2 * m_2 * R^2 #rolling body's moment of inertia with respect to its CoM (kg*m^2)
     k_m::Float64 = 0.32 #motor's torque constant (N*m)
     b_m::Float64 = 0.0189 #motor's effective damping coefficient (N*m*s/rad)
     J_m::Float64 = 0.0014 #motor's effective moment of inertia (kg*m^2)
 end
 
 @kwdef struct VehicleY
-    ω1::Float64 = 0.0 #angular velocity of main body (rad/s)
-    ω2::Float64 = 0.0 #angular velocity of rolling body (rad/s)
+    ω::Float64 = 0.0 #angular velocity of main body (rad/s)
+    v::Float64 = 0.0 #horizontal velocity of the system's origin (m/s)
     θ::Float64 = 0.0 #angle of main body with respect to vertical (rad)
-    η::Float64 = 0.0 #horizontal position (m)
-    v::Float64 = 0.0 #linear velocity (m/s)
-    ω1_dot::Float64 = 0.0 #angular acceleration of main body (rad/s^2)
-    ω2_dot::Float64 = 0.0 #angular acceleration of rolling body (rad/s^2)
-    F_21x::Float64 = 0.0 #force exerted by body 2 on body 1 along inertial frame's x-axis (N)
-    F_21z::Float64 = 0.0 #force exerted by body 2 on body 1 along inertial frame's z-axis (N)
-    F_i2x::Float64 = 0.0 #force exerted by inertial frame on body 2 along inertial frame's x-axis (N)
-    F_i2z::Float64 = 0.0 #force exerted by inertial frame on body 2 along inertial frame's z-axis (N)
-    τ_m::Float64 = 0.0 #motor torque exerted by body 1 on body 2 (N*m)
+    η::Float64 = 0.0 #horizontal position of the system's origin (m)
+    u_m::Float64 = 0.0 #motor control input (normalized)
+    τ_m::Float64 = 0.0 #motor torque (N*m)
+    ω_dot::Float64 = 0.0 #angular acceleration of main body (rad/s^2)
+    v_dot::Float64 = 0.0 #linear acceleration of the system's origin (m/s^2)
 end
 
-Modeling.X(::Vehicle) = ComponentVector( ω1 = 0.0, ω2 = 0.0, θ = 0.0, η = 0.0)
+Modeling.X(::Vehicle) = ComponentVector( ω = 0.0, v = 0.0, θ = 0.0, η = 0.0)
 Modeling.U(::Vehicle) = Ref{Float64}(0.0)
 Modeling.Y(::Vehicle) = VehicleY()
 
@@ -53,235 +51,53 @@ end
 function Modeling.f_ode!(mdl::Model{Vehicle})
 
     @unpack ẋ, x, u, parameters = mdl
-    @unpack L, R, k_m, b_m, J_m, m1, m2 = parameters
-
-    @unpack ω1, ω2, θ, η = x
-    u_m = u[]
-
-    #approximate main body's moment of inertia with respect to its CoM as
-    #that of a thin rod of length 2L
-    J1 = 1/12 * m1 * (2L)^2
-
-    #approximate rolling body's moment of inertia with respect to its CoM as the
-    #motor's effective moment of inertia plus the contribution from the body's
-    #mass, assuming a solid disk distribution
-    J2 = J_m + 1/2 * m2 * R^2
-
-    A = @SMatrix[
-        m1*L*cos(θ)  m1*R    -1         0           0     0     0
-        m1*L*sin(θ)  0       0          1           0     0     0
-        J1           0       L*cos(θ)   -L*sin(θ)   0     0     1
-        0            m2*R    1          0           -1    0     0
-        0            0       0          -1          0     1     0
-        0            J2      0          0           R     0     -1
-        0            0       0          0           0     0     1
-    ]
-
-    b = @SVector[
-        m1*L*ω1^2*sin(θ),
-        m1*g - m1*L*ω1^2*cos(θ),
-        0,
-        0,
-        m2*g,
-        0,
-        k_m * u_m - b_m * (ω2 - ω1)
-    ]
-
-    ω1_dot, ω2_dot, F_21x, F_21z, F_i2x, F_i2z, τ_m = A\b
-    v = ω2 * R
-
-    θ_dot = ω1
-    η_dot = v
-
-    ẋ.ω1 = ω1_dot
-    ẋ.ω2 = ω2_dot
-    ẋ.θ = θ_dot
-    ẋ.η = η_dot
-
-    mdl.y = VehicleY(; ω1, ω2, θ, η, v, ω1_dot, ω2_dot, F_21x, F_21z, F_i2x, F_i2z, τ_m)
-
-end
-
-
-# function Modeling.f_ode!(mdl::Model{Vehicle})
-
-#     @unpack ẋ, x, u, parameters = mdl
-#     @unpack L, R, k_m, b_m, J_m, m1, m2 = parameters
-
-#     @unpack ω1, ω2, θ, η = x
-#     u_m = u[]
-
-#     #approximate main body's moment of inertia with respect to its CoM as
-#     #that of a thin rod of length 2L
-#     J1 = 1/12 * m1 * (2L)^2
-
-#     #approximate rolling body's moment of inertia with respect to its CoM as the
-#     #motor's effective moment of inertia plus the contribution from the body's
-#     #mass, assuming a solid disk distribution
-#     J2 = J_m + 1/2 * m2 * R^2
-
-#     A = @SMatrix[
-#         m1*L*cos(θ)  m1*R    -1         0           0     0     0
-#         m1*L*sin(θ)  0       0          1           0     0     0
-#         J1           0       L*cos(θ)   -L*sin(θ)   0     0     1
-#         0            m2*R    1          0           -1    0     0
-#         0            0       0          -1          0     1     0
-#         0            J2      0          0           R     0     -1
-#         0            0       0          0           0     0     1
-#     ]
-
-#     b = @SVector[
-#         m1*L*ω1^2*sin(θ),
-#         m1*g - m1*L*ω1^2*cos(θ),
-#         0,
-#         0,
-#         m2*g,
-#         0,
-#         k_m * u_m - b_m * (ω2 - ω1)
-#     ]
-
-#     ω1_dot, ω2_dot, F_21x, F_21z, F_i2x, F_i2z, τ_m = A\b
-#     v = ω2 * R
-
-#     θ_dot = ω1
-#     η_dot = v
-
-#     ẋ.ω1 = ω1_dot
-#     ẋ.ω2 = ω2_dot
-#     ẋ.θ = θ_dot
-#     ẋ.η = η_dot
-
-#     mdl.y = VehicleY(; ω1, ω2, θ, η, v, ω1_dot, ω2_dot, F_21x, F_21z, F_i2x, F_i2z, τ_m)
-
-# end
-
-@no_step Vehicle
-@no_periodic Vehicle
-
-
-
-################################################################################
-################################## Vehicle2 #####################################
-
-#body 1: main body, comprising the vehicle chassis and the DC motor's case and stator
-#body 2: rolling body, comprising the wheel, axle, and the DC motor's rotor.
-
-@kwdef struct Vehicle2 <: ModelDefinition
-    L::Float64 = 0.15 #distance from main body's origin to its CoM (m)
-    R::Float64 = 0.05 #wheel radius (m)
-    m1::Float64 = 0.5 #mass of main body (kg)
-    m2::Float64 = 0.1 #mass of rolling body (kg)
-    k_m::Float64 = 0.32 #motor's torque constant (N*m)
-    b_m::Float64 = 0.0189 #motor's effective damping coefficient (N*m*s/rad)
-    J_m::Float64 = 0.0014 #motor's effective moment of inertia (kg*m^2)
-end
-
-@kwdef struct Vehicle2Y
-    ω::Float64 = 0.0 #angular velocity of main body (rad/s)
-    v::Float64 = 0.0 #horizontal velocity of the system's origin (m/s)
-    θ::Float64 = 0.0 #angle of main body with respect to vertical (rad)
-    η::Float64 = 0.0 #horizontal position of the system's origin (m)
-    τ_m::Float64 = 0.0 #motor torque exerted by body 1 on body 2 (N*m)
-    ω_dot::Float64 = 0.0 #angular acceleration of main body (rad/s^2)
-    v_dot::Float64 = 0.0 #linear acceleration of the system's origin (m/s^2)
-end
-
-Modeling.X(::Vehicle2) = ComponentVector( ω = 0.0, v = 0.0, θ = 0.0, η = 0.0)
-Modeling.U(::Vehicle2) = Ref{Float64}(0.0)
-Modeling.Y(::Vehicle2) = Vehicle2Y()
-
-#initialize vehicle at static vertical equilibrium
-function Modeling.init!(mdl::Model{Vehicle2})
-    mdl.x .= 0
-    mdl.u[] = 0
-    f_ode!(mdl)
-end
-
-function Modeling.f_ode!(mdl::Model{Vehicle2})
-
-    @unpack ẋ, x, u, parameters = mdl
-    @unpack L, R, k_m, b_m, J_m, m1, m2 = parameters
+    @unpack L, R, m_1, m_2, J_1, J_2, k_m, b_m, J_m = parameters
 
     @unpack ω, v, θ, η = x
     u_m = u[]
+    ω_m = v / R - ω
+    τ_m_ss = k_m * u_m - b_m * ω_m
 
-    #approximate main body's moment of inertia with respect to its CoM as
-    #that of a thin rod of length 2L
-    @show J1 = 1/12 * m1 * (2L)^2
+    J = m_1 * L^2 + J_1 + J_m
+    M = m_1 + m_2 + (J_2 + J_m) / R^2
+    K = m_1 * L
 
-    #approximate rolling body's moment of inertia with respect to its CoM as the
-    #motor's effective moment of inertia plus the contribution from the body's
-    #mass, assuming a solid disk distribution
-    @show J2 = J_m + 1/2 * m2 * R^2
-
-    τ_m = u_m*k_m - b_m*(v/R - ω)
+    sθ = sin(θ)
+    cθ = cos(θ)
 
     A = @SMatrix[
-        m1*L^2 + J1     m1*L*cos(θ)
-        m1*L*cos(θ)     m1 + m2 + J2/R^2
+        J                   K * cθ - J_m / R
+        K * cθ - J_m / R    M
     ]
 
     b = @SVector[
-        -τ_m + m1*g*L*sin(θ)
-        τ_m/R + m1*L*ω^2*sin(θ)
+        -τ_m_ss + K * g * sθ
+        1/R * τ_m_ss + K * ω^2 * sθ
     ]
 
     ω_dot, v_dot = A\b
+    ω_m_dot = v_dot/R - ω_dot
 
     θ_dot = ω
     η_dot = v
+
+    τ_m = τ_m_ss - J_m * ω_m_dot
 
     ẋ.ω = ω_dot
     ẋ.v = v_dot
     ẋ.θ = θ_dot
     ẋ.η = η_dot
 
-    mdl.y = Vehicle2Y(; ω, v, θ, η, τ_m, ω_dot, v_dot)
+    mdl.y = VehicleY(; ω, v, θ, η, u_m, τ_m, ω_dot, v_dot)
 
 end
 
-@no_step Vehicle2
-@no_periodic Vehicle2
+@no_step Vehicle
+@no_periodic Vehicle
 
 function test_vehicle()
 
     mdl = Vehicle() |> Model
-
-    mdl.u[] = 0
-    mdl.x.ω1 = 0.0
-    mdl.x.ω2 = 0
-    mdl.x.θ = 0.0
-    f_ode!(mdl)
-    @show mdl.y
-
-    mdl.u[] = 0.118125
-    mdl.x.ω1 = 0.0
-    mdl.x.ω2 = 2.0
-    mdl.x.θ = 0.0
-    f_ode!(mdl)
-    @show mdl.y
-
-    mdl.u[] = 1.0
-    mdl.x.ω1 = 0.0
-    mdl.x.ω2 = 17.0
-    mdl.x.θ = 0.0
-    f_ode!(mdl)
-    @show mdl.y
-
-    mdl.u[] = 0.2
-    mdl.x.ω1 = 0.1
-    mdl.x.ω2 = 2.0
-    mdl.x.θ = 0.05
-    f_ode!(mdl)
-    @show mdl.y
-
-    nothing
-
-end
-
-function test_vehicle2()
-
-    mdl = Vehicle2() |> Model
 
     mdl.u[] = 0
     mdl.x.ω = 0.0
@@ -290,30 +106,26 @@ function test_vehicle2()
     f_ode!(mdl)
     @show mdl.y
 
-    ω2 = 2.0
-    v = ω2 * mdl.parameters.R
     mdl.u[] = 0.118125
     mdl.x.ω = 0.0
-    mdl.x.v = v
+    mdl.x.v = 0.1
     mdl.x.θ = 0.0
     f_ode!(mdl)
     @show mdl.y
 
-    ω2 = 17.0
-    v = ω2 * mdl.parameters.R
-    mdl.u[] = 1.0
+    mdl.u[] = 0.247714904
     mdl.x.ω = 0.0
-    mdl.x.v = v
-    mdl.x.θ = 0.0
+    mdl.x.v = 0.1
+    mdl.x.θ = 0.1
     f_ode!(mdl)
     @show mdl.y
 
-    mdl.u[] = 0.2
-    mdl.x.ω = 0.1
-    mdl.x.v = 2.0 * mdl.parameters.R
-    mdl.x.θ = 0.05
-    f_ode!(mdl)
-    @show mdl.y
+    # mdl.u[] = 0.2
+    # mdl.x.ω = 0.1
+    # mdl.x.v = 0.1
+    # mdl.x.θ = 0.05
+    # f_ode!(mdl)
+    # @show mdl.y
 
     nothing
 end
@@ -356,12 +168,11 @@ end
 ################################ State Space ###################################
 
 #TODO: find the maximum steady-state velocity for abs(u) = 1
-#TODO: keep τ_m in YStateSpace to check for saturation during controller design,
 #we need to leave some margin for stabilization
 
 @kwdef struct XStateSpace <: FieldVector{4, Float64}
-    ω1::Float64 = 0.0
-    ω2::Float64 = 0.0
+    ω::Float64 = 0.0
+    v::Float64 = 0.0
     θ::Float64 = 0.0
     η::Float64 = 0.0
 end
@@ -370,23 +181,22 @@ end
     motor::Float64 = 0.0
 end
 
-@kwdef struct YStateSpace <: FieldVector{6, Float64}
-    ω1::Float64 = 0.0
-    ω2::Float64 = 0.0
+@kwdef struct YStateSpace <: FieldVector{5, Float64}
+    ω::Float64 = 0.0
+    v::Float64 = 0.0
     θ::Float64 = 0.0
     η::Float64 = 0.0
-    v::Float64 = 0.0
     τ_m::Float64 = 0.0
 end
 
 function get_ẋ_ss(mdl::Model{Vehicle})
-    @unpack ω1, ω2, θ, η = mdl.ẋ
-    XStateSpace(; ω1, ω2, θ, η)
+    @unpack ω, v, θ, η = mdl.ẋ
+    XStateSpace(; ω, v, θ, η)
 end
 
 function get_x_ss(mdl::Model{Vehicle})
-    @unpack ω1, ω2, θ, η = mdl.x
-    XStateSpace(; ω1, ω2, θ, η)
+    @unpack ω, v, θ, η = mdl.x
+    XStateSpace(; ω, v, θ, η)
 end
 
 function get_u_ss(mdl::Model{Vehicle})
@@ -394,15 +204,15 @@ function get_u_ss(mdl::Model{Vehicle})
 end
 
 function get_y_ss(mdl::Model{Vehicle})
-    @unpack ω1, ω2, θ, η, v, τ_m = mdl.y
-    YStateSpace(; ω1, ω2, θ, η, v, τ_m)
+    @unpack ω, v, θ, η, τ_m = mdl.y
+    YStateSpace(; ω, v, θ, η, τ_m)
 end
 
 assign_x_ss!(mdl::Model{Vehicle}, x::AbstractVector{<:Real}) = assign_x_ss!(mdl, XStateSpace(x))
 
 function assign_x_ss!(mdl::Model{Vehicle}, x::XStateSpace)
-    @unpack ω1, ω2, θ, η = x
-    @pack! mdl.x = ω1, ω2, θ, η
+    @unpack ω, v, θ, η = x
+    @pack! mdl.x = ω, v, θ, η
 end
 
 assign_u_ss!(mdl::Model{Vehicle}, u::AbstractVector{<:Real}) = assign_u_ss!(mdl, UStateSpace(u[1]))
@@ -455,6 +265,4 @@ struct Controller <: ModelDefinition end
     controller::C = Controller()
 end
 
-
-
-end #module
+end
